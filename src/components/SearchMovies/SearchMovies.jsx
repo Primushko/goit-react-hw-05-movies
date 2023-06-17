ca708d8e2b9f4d018e62a55e380e72d85a60f204
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-06-17 14:49:38 +0300

    movies

---
+++ src/components/SearchMovies/SearchMovies.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import { toast } from 'react-hot-toast';
+import { Button, Form, Input } from './SearchMovies.styled';
+const SearchMovies = ({ onSubmit }) => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    const query = e.target.elements.query.value;
+    if (!query) {
+      toast.error('Please enter something');
+      return;
+    }
+
+    onSubmit(query);
+    e.target.reset();
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Input name="query" type="text" placeholder="Search movies" />
+      <Button type="submit">Search</Button>
+    </Form>
+  );
+};
+
+SearchMovies.propTypes = { onSubmit: PropTypes.func.isRequired };
+
+export default SearchMovies;
