e5522405d1a408e167df3a352de5d90d2511ef52
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-06-22 01:22:32 +0300

    робота над помилками 1/22____22/06/23

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,22 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { SearchForm, Input, Button } from './Form.styled';
-//  компонент `Form` -відображає форму пошуку фільмів.
-const Form = ({ searchMovies }) => {
-  // query - значення поля вводу
-  const [query, setQuery] = useState('');
-  // функції `handleInputChange` -викликається при зміні значення в полі вводу.
-  // отримує подію `event` і встановлює нове значення `query` за допомогою функції `setQuery`.
+
+const Form = ({ searchMovies, query }) => {
+  const [inputQuery, setInputQuery] = useState('');
+
+  useEffect(() => {
+    setInputQuery(query);
+  }, [query]);
+
   const handleInputChange = event => {
-    setQuery(event.target.value);
+    setInputQuery(event.target.value);
   };
-  // функції `handleSubmit`- викликається при поданні форми.
-  // отримує подію `event` і викликає функцію `searchMovies` зі значенням `query`,
+
   const handleSubmit = event => {
-    //  `event.preventDefault()` викликається для того,щоб запобігти перезавантаженню
-    // сторінки після натискання кнопки "Submit"
     event.preventDefault();
-    searchMovies(query.toLowerCase());
+    searchMovies(inputQuery.toLowerCase());
   };
 
   return (
@@ -25,16 +24,17 @@ const Form = ({ searchMovies }) => {
         type="text"
         name="query"
         autoFocus
-        value={query}
+        value={inputQuery}
         onChange={handleInputChange}
       />
       <Button type="submit">Search</Button>
     </SearchForm>
   );
 };
-//  `PropTypes` забезпечує контроль типів переданих пропсів у компонент `Form`.
+
 Form.propTypes = {
   searchMovies: PropTypes.func.isRequired,
+  query: PropTypes.string.isRequired,
 };
 
 export default Form;
--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,39 +1,61 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Loader from 'components/Loader/Loader';
 import EditorList from 'pages/EditorList/EditorList';
-// Імпорт компонента `Form` з модуля `'components/Form/Form'`бпредставляє форму пошуку фільмів.
 import Form from 'components/Form/Form';
-//  виконання запиту пошуку фільмів за ключовим словом.
 import { fetchSearchByKeyword } from 'services/Api';
 
-// `Movies`-відповідає за відображення списку фільмів,отриманих після пошуку за ключовим словом.
 const Movies = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
   const [searchFilms, setSearchFilms] = useState([]);
   const [loading, setLoading] = useState(false);
   const [noMoviesText, setNoMoviesText] = useState(false);
-  // функція `searchMovies` викликається при виконанні пошуку фільмів
-  // отримує `queryMovie` - ключове слово для пошуку фільмів.
-  const searchMovies = queryMovie => {
-    setLoading(true);
-    // виконується запит до API з використанням `fetchSearchByKeyword`
-    fetchSearchByKeyword(queryMovie)
-      .then(searchResults => {
-        // Результати зберігаються в стані `searchFilms`
-        setSearchFilms(searchResults);
-        setNoMoviesText(searchResults.length === 0);
-      })
-      .catch(error => {
-        console.log(error);
-      })
-      // стан `loading` завжди оновлюється на `false` в кінці.
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  const [query, setQuery] = useState('');
+
+  const searchMovies = useCallback(
+    queryMovie => {
+      setLoading(true);
+      fetchSearchByKeyword(queryMovie)
+        .then(searchResults => {
+          setSearchFilms(searchResults);
+          setNoMoviesText(searchResults.length === 0);
+          navigate(
+            queryMovie
+              ? `/movies?query=${encodeURIComponent(queryMovie)}`
+              : '/movies'
+          );
+        })
+        .catch(error => {
+          console.log(error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    },
+    [navigate]
+  );
+
+  useEffect(() => {
+    const queryMovie = new URLSearchParams(location.search).get('query');
+    if (queryMovie) {
+      setQuery(queryMovie);
+    }
+    searchMovies(queryMovie);
+  }, [location.search, searchMovies]);
+
+  useEffect(() => {
+    if (location.pathname === '/movies') {
+      const queryMovie = new URLSearchParams(location.search).get('query');
+      if (!queryMovie) {
+        setSearchFilms([]);
+      }
+    }
+  }, [location.pathname, location.search]);
 
   return (
     <main>
-      <Form searchMovies={searchMovies} />
+      <Form searchMovies={searchMovies} query={query} />
       {loading && <Loader />}
       {noMoviesText && (
         <p>THERE IS NO MOVIES WITH THIS REQUEST. PLEASE, TRY AGAIN!</p>
