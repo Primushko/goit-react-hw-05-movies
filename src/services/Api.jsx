f46a495c65f6745a3d1bc4f9ba0ed3b10977c8e3
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-06-21 23:00:24 +0300

    update api
    update api

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -3,18 +3,21 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 // ключ
 const API_KEY = 'd6edcad997ee9c808a30a1ff00b87277';
+
 // список найпопулярніших фільмів на сьогодні для створення колекції на головній сторінці.
 export const fetchTrending = async () => {
   const response = await axios.get(`trending/movie/day?api_key=${API_KEY}`);
   return response.data.results;
 };
+
 // пошук фільму за ключовим словом на сторінці фільмів.(keyword)
-export const fetchSearchByKeyword = async keyword => {
+export const fetchSearchByKeyword = async query => {
   const response = await axios.get(
-    `search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${keyword}`
+    `search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
   );
   return response.data.results;
 };
+
 // запит повної інформації про фільм для сторінки кінофільму.
 export const fetchMovieDetails = async movieId => {
   const response = await axios.get(
@@ -22,6 +25,7 @@ export const fetchMovieDetails = async movieId => {
   );
   return response.data;
 };
+
 // запит інформації про акторський склад для сторінки кінофільму.
 export const fetchActors = async movieId => {
   const response = await axios.get(
@@ -29,6 +33,7 @@ export const fetchActors = async movieId => {
   );
   return response.data.cast;
 };
+
 // запит оглядів для сторінки кінофільму.
 export const fetchReviews = async movieId => {
   const response = await axios.get(
